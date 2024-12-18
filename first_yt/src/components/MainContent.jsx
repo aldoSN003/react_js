91f1eff3ae18ddc55c710d6783718be4036d85a9
aldoSNN003 <j.aldosn05@gmail.com>
2024-12-17 20:13:41 -0600

    test

--- first_yt/src/components/MainContent.jsx
+++ first_yt/src/components/MainContent.jsx
@@ -1,10 +1,10 @@
 import "./../index.css"
 export default function MainContent() {
     return (
-        <main className="content-container p-6 ml-5 mt-10 ">
+        <main className="content-container p-6 ml-5 mt-10  ">
             <h1>Fun Facts about React</h1>
 
-            <ul className="mt-4 facts-list">
+            <ul className="mt-4 facts-list ">
                 <li>React is a JavaScript library for building user interfaces.</li>
                 <li>It was developed by Facebook and is maintained by Facebook and a community of individual
                     developers.
